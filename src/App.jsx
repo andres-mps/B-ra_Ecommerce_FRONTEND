6c9143050ea01238645500803734054c58a1e21e
andres-mps <andresmps@gmail.com>
2023-06-14 15:33:28 -0300

    home-base

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <p style={{ fontFamily: "HelveticaEx", fontSize: "50px" }}>Beers</p>
-      <p style={{ fontFamily: "HelveticaExLight", fontSize: "50px" }}>Beers</p>
+      <Home />
     </>
   );
 }
