86780104b14b677fb60e4857971112785a79cebe
andres-mps <andresmps@gmail.com>
2023-06-14 16:11:58 -0300

    product component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,17 @@
 import "./Home.css";
+import Product from "../components/Product";
 
 function Home() {
   return (
     <main>
       <section className="hero container-fluid">
-        <div>{/* <img src="/public/img/home/header_home.webp" alt="" /> */}</div>
+        <div className="imgContainer">
+          <img className="header_img" src="/public/img/home/header_home.webp" alt="" />
+        </div>
       </section>
       <section id="featured_products" className="container-fluid">
         <div className="row">
-          <div className="col-6 col-md-3">
-            <div className="d-flex flex-column text-center">
-              <img src="/public/img/products/Action-Directe_44cl_can.webp" alt="" />
-              <p className="productName">Action Direct</p>
-              <p className="productPrice">41.60 USD</p>
-            </div>
-          </div>
-          <div className="col-6 col-md-3">
-            <div className="d-flex flex-column text-center">
-              <img src="/public/img/products/TroubleJuice-44cl.webp" alt="" />
-              <p className="productName">Action Direct</p>
-              <p className="productPrice">41.60 USD</p>
-            </div>
-          </div>
-          <div className="col-6 col-md-3">
-            <div className="d-flex flex-column text-center">
-              <img src="/public/img/products/brokilde-brown.webp" alt="" />
-              <p className="productName">Action Direct</p>
-              <p className="productPrice">41.60 USD</p>
-            </div>
-          </div>
-          <div className="col-6 col-md-3">
-            <div className="d-flex flex-column text-center">
-              <img src="/public/img/products/sovs.webp" alt="" />
-              <p className="productName">Action Direct</p>
-              <p className="productPrice">41.60 USD</p>
-            </div>
-          </div>
+          <Product />
         </div>
       </section>
     </main>
