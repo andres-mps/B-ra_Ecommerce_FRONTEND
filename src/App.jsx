33c3feb688d0ae33c936e719b45b28dc9e6a8f32
gbascans <gbascansc@gmail.com>
2023-06-15 10:45:50 -0300

    Correccion de ruta Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AboutThisProject from "./pages/AboutThisProject";
-import Product from "./pages/Product";
+import ProductDetail from "./pages/ProductDetail";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<ProductDetail />} />
         <Route path="/about-this-project" element={<AboutThisProject />} />
       </Routes>
     </>
