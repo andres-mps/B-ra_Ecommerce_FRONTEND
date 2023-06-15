111ae54daf64d6c3ba1799b789aad59cd43c48b6
Guillermo Sanchez <guillesan3@gmail.com>
2023-06-15 19:08:32 -0300

    Fetch categories en pagina Style

--- src/pages/Style.jsx
+++ src/pages/Style.jsx
@@ -6,6 +6,7 @@ import "./Style.css";
 
 function Style() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     async function getProductInfo() {
@@ -18,24 +19,30 @@ function Style() {
     getProductInfo();
   }, []);
 
+  useEffect(() => {
+    async function getCategories() {
+      const response = await axios({
+        method: "GET",
+        url: `http://localhost:3000/products/categories`,
+      });
+      setCategories(response.data);
+    }
+    getCategories();
+  }, []);
   return (
     <section>
       <h1 className="styles-header">Beers</h1>
       <div className="container-fluid d-flex flex-row mb-5">
         <div className="row">
           <div className="styles-list-container px-5">
-            <NavLink className="text-decoration-none">
-              <h2 className="styles-list-header">IPA</h2>
-            </NavLink>
-            <NavLink className="text-decoration-none">
-              <h2 className="styles-list-header">Sour</h2>
-            </NavLink>
-            <NavLink className="text-decoration-none">
-              <h2 className="styles-list-header">Stout</h2>
-            </NavLink>
-            <NavLink className="text-decoration-none">
-              <h2 className="styles-list-header">Lager & Pilsner</h2>
-            </NavLink>
+            {categories &&
+              categories.map((category) => {
+                return (
+                  <NavLink to={`/styles/${category.name}`} className="text-decoration-none">
+                    <h2 className="styles-list-header">{category.name}</h2>
+                  </NavLink>
+                );
+              })}
           </div>
           {products &&
             products.map((product) => {
