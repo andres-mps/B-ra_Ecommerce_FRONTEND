4b38c969dee1c400a26c9b235ce3d18844bcdc2b
gbascans <gbascansc@gmail.com>
2023-06-16 13:04:41 -0300

    Styles parametrizado

--- src/pages/Style.jsx
+++ src/pages/Style.jsx
@@ -3,24 +3,42 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Style.css";
+import { useParams } from "react-router-dom";
 
 function Style() {
-  const [style, setStyle] = useState("");
-  console.log(style);
+  const params = useParams();
+  const [err, setErr] = useState(null);
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
+    async function getAllProductInfo() {
+      const response = await axios({
+        method: "GET",
+        url: `http://localhost:3000/products`,
+      });
+      setErr(null);
+      return setProducts(response.data);
+    }
+
     async function getProductInfo() {
       const response = await axios({
         method: "GET",
-        url: `http://localhost:3000/products/${style}`,
+        url: `http://localhost:3000/categories/${params.style}`,
       });
-      setProducts(response.data);
+      if (response.data.type === "err") {
+        return setErr(response.data.content);
+      }
+      setErr(null);
+      return setProducts(response.data);
     }
 
-    getProductInfo();
-  }, [style]);
+    if (params.style === undefined) {
+      getAllProductInfo();
+    } else {
+      getProductInfo();
+    }
+  }, [params.style]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -39,9 +57,9 @@ function Style() {
   }, []);
   return (
     <section>
-      <h1 className="styles-header" onClick={() => setStyle("")}>
-        Beers
-      </h1>
+      <NavLink to="/styles" className="text-decoration-none text-black">
+        <h1 className="styles-header">Beers</h1>
+      </NavLink>
       <div className="container-fluid">
         <div className="styles-list-container px-5">
           {categories &&
@@ -50,13 +68,19 @@ function Style() {
                 <NavLink
                   key={category.id}
                   className="text-decoration-none"
-                  onClick={() => setStyle(category.name)}
+                  to={`/styles/${category.slug}`}
                 >
                   <h2 className="styles-list-header">{category.name}</h2>
                 </NavLink>
               );
             })}
         </div>
+        {err && (
+          <div id="error" class="alert alert-danger text-center" role="alert">
+            {err}
+          </div>
+        )}
+
         <div className="row align-items-end">
           {products &&
             products.map((product) => {
