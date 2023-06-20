1f79b888e7a1584589f015a7556b20bf4516b799
Guillermo Sanchez <guillesan3@gmail.com>
2023-06-20 15:48:05 -0300

    Pagina agegate, falta aplicar y corregir scroll

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Checkout from "./pages/Checkout";
 import Page from "./pages/Page";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AgeGate from "./pages/AgeGate";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="about-this-project" element={<AboutThisProject />} />
         </Route>
 
+        <Route path="age-gate" element={<AgeGate />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </>
