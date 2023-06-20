391630f0218004720a852450280431c1579bb56f
gbascans <gbascansc@gmail.com>
2023-06-20 18:06:19 -0300

    Update CheckoutItems.jsx

--- src/components/CheckoutItems.jsx
+++ src/components/CheckoutItems.jsx
@@ -1,11 +1,12 @@
 function CheckoutItems({ beer }) {
   return (
     <>
-      <div>
+      <div className="d-flex align-items-center gap-3">
         <div className="checkout-product-photo-container">
           <img src={beer} alt="" className="checkout-product-photo" />
           <span className="checkout-product-span">1</span>
         </div>
+        <p className="checkout-regular-text fw-bold">Action Direct</p>
       </div>
       <p>kr 36.00</p>
     </>
