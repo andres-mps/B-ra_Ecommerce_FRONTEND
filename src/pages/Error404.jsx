4d7ebf35a9fe5c22ee17e4d2d33c2952e2b262f3
Emiliano <emilianogaucher@gmail.com>
2023-06-23 11:06:25 -0300

    Agrego 404

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Error404.css';
+import { NavLink } from 'react-router-dom';
+
+function Error404() {
+  return (
+    <main className='main-404'>
+      <div className='background-img'>
+        <img
+          src="https://images.unsplash.com/photo-1532634740-6837b2a21d39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          alt=""
+          className='bg-image'
+        />
+      </div>
+      <div className='text-404'>
+        <p>404</p>
+        <p>Page not found</p>
+        <NavLink to="/home">
+        <button>Continue shopping</button>
+        </NavLink>
+      </div>
+    </main>
+  );
+}
+
+export default Error404;
